d970f3e34ac7b3a213ece44d6a0f0adaac68700d
Guillermo Anta <guillermo.anta@telecomboutique.com>
2025-03-06 11:31:18 +0100

    Refactor bmi.py

--- bmi.py
+++ bmi.py
@@ -5,100 +5,96 @@ import numpy as np
 import functions 
 from flask import render_template
 
-
 app = flask.Flask("bmi")
    
+# Constants for BMI ranges
+BMI_RANGES = {
+    (0, 18.5): "You are underweight",
+    (18.5, 24.9): "Your weight is normal",
+    (24.9, 29.9): "You are in overweight",
+    (30, float('inf')): "You are obese"
+}
 
+def get_health_message(bmi):
+    """Helper function to determine health message based on BMI"""
+    for (lower, upper), message in BMI_RANGES.items():
+        if lower <= bmi < upper:
+            return message
+    return "Please, introduce correct values"
 
 @app.route("/")
 def homepage():
     return functions.get_html("bmi")
 
-
 @app.route("/result")
 def bmi_formula():
-    try: #preventing errors
-        user_weight = flask.request.args.get("weight_input")
-        user_height = flask.request.args.get("height_input")
-        user_name = flask.request.args.get("username") #until here we take the values itslef, not yet part of an object
-        if len(user_weight) < 1 or len(user_height) < 1 or len(user_name) < 1: #just making sure that the user fills all the fields
-            route = functions.get_html("/result")
-            route = route.replace("$BMIRESULT$", "Please, introduce correct values")
-            route = route.replace("$HEALTHRESULT$","")
-            return route
-            
-        user_bmi = float(user_weight) / ((int(user_height) / 100) * (float(user_height) / 100)) #simply the formula with the values from the user
-        if user_bmi > 100 or user_bmi < 10:
-            route = functions.get_html("/result")
-            route = route.replace("$BMIRESULT$", "Please, introduce correct values")
-            route = route.replace("$HEALTHRESULT$","")
-            return route
-        rounded_bmi = round(user_bmi,2)#rounding to 2 decimals the result
+    try:
+        user_weight = flask.request.args.get("weight_input", "")
+        user_height = flask.request.args.get("height_input", "")
+        user_name = flask.request.args.get("username", "")
 
-        user_object = functions.User(user_name, rounded_bmi) #creating the object from class user
-        object_bmi = float(user_object.bmi)  #enclosing the bmi of the object in a value
-        
-        user_object.storing_data() #calling the storing_data method to store the data in the db
+        # Input validation
+        if not all([user_weight, user_height, user_name]):
+            raise ValueError("Missing required fields")
 
-        #now the conditional logic for the health message:
-        health_message = ""
-        if int(user_bmi) < 18.5:
-            health_message = "You are underweight"
-        elif int(user_bmi) > 18.5 and int(user_bmi) < 24.9:
-            health_message = "Your weight is normal"
-        elif int(user_bmi) > 24.9 and int(user_bmi) < 29.9:
-            health_message = "You are in overweight"
-        elif int(user_bmi) > 30:
-            health_message = "You are obese"
+        # Convert and validate numeric inputs
+        weight = float(user_weight)
+        height = float(user_height)
         
+        # Calculate BMI
+        height_meters = height / 100
+        user_bmi = weight / (height_meters * height_meters)
+
+        # Validate BMI range
+        if not 10 <= user_bmi <= 100:
+            raise ValueError("BMI out of realistic range")
+
+        rounded_bmi = round(user_bmi, 2)
         
+        # Create and store user data
+        user_object = functions.User(user_name, rounded_bmi)
+        user_object.storing_data()
+
+        # Get health message
+        health_message = get_health_message(user_bmi)
+        
+        # Prepare response
         route = functions.get_html("/result")
-        route = route.replace("$BMIRESULT$", "Your BMI is: " + str(object_bmi)) #displaying the bmi
-        route = route.replace("$HEALTHRESULT$",health_message) #displaying the health message
+        route = route.replace("$BMIRESULT$", f"Your BMI is: {rounded_bmi}")
+        route = route.replace("$HEALTHRESULT$", health_message)
         return route
-    except: #if there are wrong or empty values, here is the path:
-            route = functions.get_html("/result")
-            route = route.replace("$BMIRESULT$", "Please, introduce correct values")
-            route = route.replace("$HEALTHRESULT$","")
-            return route
 
-
+    except (ValueError, TypeError) as e:
+        route = functions.get_html("/result")
+        route = route.replace("$BMIRESULT$", "Please, introduce correct values")
+        route = route.replace("$HEALTHRESULT$", "")
+        return route
 
 @app.route("/statistics")
 def statistics_page():  
-    users_bmi = functions.get_numbers() #enclosing the users bmi array in a value
-    piechart = functions.piechartpx()
-    
     try:
-        if len(users_bmi) == 0:
-            message = "The database is empty"
+        users_bmi = functions.get_numbers()
+        piechart = functions.piechartpx()
+        
+        if not users_bmi:
             route = functions.get_html("statistics")
             route = route.replace("{{ plot_html | safe }}", "")
-            route = route.replace("{{ usersdata }}",message)
+            route = route.replace("{{ usersdata }}", "The database is empty")
             return route
         
-        users_average = round(np.mean(users_bmi),2) #calling np to get the average
-
-        health_status = "" #initializating empty string and again, conditional logic:
-
-        if users_average < 18.5:
-            health_status = "underweight"
-        elif users_average > 18.5 and users_average < 24.9:
-            health_status = "normal"
-        elif users_average > 24.9 and users_average < 29.9:
-            health_status = "overweight"
-        else:
-            health_status = "obese"
-        users_data = "The average of the users of this app is " + str(users_average) + " , which is " + health_status 
+        users_average = round(np.mean(users_bmi), 2)
+        health_status = get_health_message(users_average).lower().split()[-1]
+        
+        users_data = f"The average of the users of this app is {users_average}, which is {health_status}"
+        
         route = functions.get_html("statistics")
-        route = route.replace("{{ plot_html | safe }}", piechart) 
+        route = route.replace("{{ plot_html | safe }}", piechart)
         route = route.replace("{{ usersdata }}", users_data)
         return route
 
-    except: #if the database is empty, this is the path:
-        users_bmi = 0 
+    except Exception:
         route = functions.get_html("statistics")
-        route = route.replace("{{ usersdata }}","There are no users yet in our database")
+        route = route.replace("{{ usersdata }}", "There are no users yet in our database")
         return route
 
 
